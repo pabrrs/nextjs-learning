c9d87f3712c8eed59006f0bb545b52ac027d620f
pabrrs <paulo.alfredo.barros@gmail.com>
2024-05-27 12:27:30 -0400

    foo

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Noto_Sans, Lusitana } from 'next/font/google';
+ 
+export const notoSans = Noto_Sans({ subsets: ['latin'], weight: '600' });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
